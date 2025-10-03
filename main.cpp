6cfe218373db56f500129be254a7edefaca943b5
Natal-hash <vinogradovvladimir417@gmail.com>
2025-10-03 19:59:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<string>
 
 int main() {
-  const chat* secret = std::getenv("PASSWORD");
+  const char* secret = std::getenv("PASSWORD");
   std::string s(secret)
   if(s = "qwerty") {
     std::cout<<"Пароль верный!!!"<<std::endl;
